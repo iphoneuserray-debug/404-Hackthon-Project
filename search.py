67d28196164e4f42072d033ced53526e0e9b35a4
Ray Dai <iphone.user.ray@gmail.com>
2025-09-07 14:42:32 +1000

    generate data

--- search.py
+++ search.py
@@ -1,19 +1,20 @@
 import pandas as pd
-import tempfile
 
-# Read the Excel file, skipping the first two rows
-# def search_excel(query):
-#     df = pd.read_excel('sensor test 1.0 .xlsx', header = 2)
-#     result = df[df['Sensor'] == query]
-#     with tempfile.NamedTemporaryFile(suffix=".xlsx", delete=False) as tmp:
-#         result.to_excel(tmp.result, index=False)
-#         tmp.close()
-#     return tmp.result
 
 def search_excel(sensor_name):
-    df = pd.read_excel('sensor test 1.0 .xlsx', header=2)
-    if 'Sensor' not in df.columns:
+    df = pd.read_excel('sensor test 1.0 .xlsx', header= 2)
+    if 'sensor' not in df.columns:
         raise ValueError("Column 'Sensor' not found")
-    result = df[df['Sensor'].astype(str) == str(sensor_name)]
+    result = df[df['sensor'].astype(str) == str(sensor_name)]
+
+    if 'i2c_functionality' in result.columns:
+        result['i2c_functionality'] = result['i2c_functionality'].apply(
+            lambda x: (
+                f"✅ {x}" if str(x).strip().lower() == 'working'
+                else f"🔘 {x}" if str(x).strip().lower() == 'offline'
+                else f"🟠 {x}" if str(x).strip().lower() == 'affected'
+                else f"❌ {x}"
+            )
+        )
     
     return result
